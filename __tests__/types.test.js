9156ee859177a71c05eabcc7fc95a8c501ff583c
Brittany Houtz <brittany.houtz@gmail.com>
2019-12-02 15:43:23 -0800

    add casters to getCaster

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -87,6 +87,8 @@ describe('validator module', () => {
 
   it('can get the right caster', () => {
     expect(getCaster(Number)).toEqual(castToNumber);
+    expect(getCaster(String)).toEqual(castToString);
+    expect(getCaster(Boolean)).toEqual(castToBoolean);
     expect(getCaster(Promise)).toBeNull();
   });
 
